6660fe74f3f884898abd9d5802cd5c5e28edc8c0
Cyto-plasm <ruparakshit25@gmail.com>
2026-02-19 23:25:31 +0530

    Upload new logical solution

---
+++ Arrays/maximuminarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of an array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for (int i = 0; i <= n-1; i++)
+    {
+        cin>>arr[i];
+    }
+    int max = arr[0];
+    for (int i = 0; i <= n-1; i++)
+    {
+        if(max < arr[i]) max = arr[i];
+    }
+    cout<<max;
+    
+}
