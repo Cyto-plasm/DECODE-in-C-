ed0c3a02473b08259cd0d09ee30b2bd27bc4c71d
Cyto-plasm <ruparakshit25@gmail.com>
2026-02-20 15:59:02 +0530

    Add INT-MIN in this problem

--- Arrays/maximuminarray.cpp
+++ Arrays/maximuminarray.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 int main(){
     int n;
@@ -10,7 +11,7 @@ int main(){
     {
         cin>>arr[i];
     }
-    int max = arr[0];
+    int max = INT_MIN;
     for (int i = 0; i <= n-1; i++)
     {
         if(max < arr[i]) max = arr[i];
