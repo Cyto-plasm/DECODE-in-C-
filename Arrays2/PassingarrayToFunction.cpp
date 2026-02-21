0cd7d537dcf3d653ce987092657a271b408b9c54
Cyto-plasm <ruparakshit25@gmail.com>
2026-02-21 18:52:16 +0530

    Add sizeof for the array

--- Arrays2/PassingarrayToFunction.cpp
+++ Arrays2/PassingarrayToFunction.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 using namespace std;
-void display(int a[]){
+void display(int a[], int size){
     for(int i = 0; i<=4; i++){
         cout<<a[i]<<" ";
     }
     cout<<endl;
     return;
 }
-void change(int b[]){
+void change(int b[], int size){
     b[0] = 100;
 }
 int main(){
     int arr[5] = {2,5,7,1,9};
     //accessing the elements of array in another function
     //updation pass by value / reference ?
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<size<<endl;
 
-    display(arr);
-    change(arr);
-    display(arr);
+    display(arr,size);
+    change(arr,size);
+    display(arr,size);
 }
