a113dc3cb45483f7b587ec5b573b4f5521a79f08
Cyto-plasm <ruparakshit25@gmail.com>
2026-02-22 17:24:18 +0530

    Implement arrays using pointers

--- Arrays2/arraysAndpointers.cpp
+++ Arrays2/arraysAndpointers.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 int main(){
     int arr[] = {4,6,2,8,3};
-    int *ptr = &arr[0]; //giving address
-    cout<<ptr<<endl;
-    ptr[0] = 10;
+    int *ptr = arr; //giving address 
     for(int i = 0; i<=4; i++){
-        cout<<ptr[i]<<" ";
+        cout<<*ptr<<" ";
+        ptr++;
     }
+    ptr = arr; //ptr is pointing to 1st element
 
+    
+    *ptr = 10; //ptr[0] = 10
+    ptr++; //ptr is pointing to 2nd element
+    *ptr = 15;
+    ptr--; //ptr is pointing to 1st element
+    cout<<endl;
+
+    for(int i = 0; i<=4; i++){
+        cout<<*ptr <<" ";
+        ptr++;
+    }
+    ptr = arr; //ptr is pointing to 1st element
+     
 }
